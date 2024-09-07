c32a77382a502c546d9059a2b986bbea25d325b8
Senol Demir <66261119+SenolDemir@users.noreply.github.com>
2024-09-07 22:39:24 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 public static void main(String[] args)
     {
-        System.out.println("Now It is Jenkins job  with Jenkinsfile");
+        System.out.println("Now It is Jenkins job  with jenkinsfile");
         System.out.println("This is for Github Webhook");
       
     }
